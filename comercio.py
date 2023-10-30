4d2bebd9fbb61f2161c7e35145bdbdec92e4ac01
Tomás <99937483+TomasLira@users.noreply.github.com>
2023-10-30 15:47:43 -0300

    add algumas funcionalidades

--- comercio.py
+++ comercio.py
@@ -13,16 +13,39 @@ class Produto():
     def decrease_quantity(self, amount):
         ...
     
-class Book(Produto):
-    def __init__(self, name, price, quantity) -> None:
-        super().__init__(name, price, quantity)
+class Chip(Produto):
+    def __init__(self, name, price, quantity,bar_code) -> None:
+        super().__init__(name, price, quantity,bar_code)
 
 class Notebook(Produto):
-    def __init__(self, name, price, quantity) -> None:
-        super().__init__(name, price, quantity)
+    def __init__(self, name, price, quantity,bar_code) -> None:
+        super().__init__(name, price, quantity,bar_code)
 
 class Instrument(Produto):
-    def __init__(self, name, price, quantity) -> None:
-        super().__init__(name, price, quantity)
+    def __init__(self, name, price, quantity,bar_code) -> None:
+        super().__init__(name, price, quantity,bar_code)
 
+class Inventario():
+    def __init__(self) -> None:
+        self.products = []
+    
+    def add_to_inventory(self,product):
+        self.products.append(product)
+    def check_products_inventory(self):
+        for product in self.products:
+            print(product.name)
+    def check_barcode_inventory(self):
+        for product in self.products:
+            print(product.bar_code)
 
+  
+
+a = Chip('chip','12','34','anfian#cms')
+b = Instrument('piano','12','3','adf2f#cms')
+inv = Inventario()
+inv.add_to_inventory(a)
+inv.add_to_inventory(b)
+inv.check_products_inventory()
+
+    
+    
