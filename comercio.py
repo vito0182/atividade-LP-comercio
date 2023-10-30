df3ec72a7f2d6fb4893b22f8f7269b87d4074e53
Tomás <99937483+TomasLira@users.noreply.github.com>
2023-10-30 16:45:48 -0300

    add mais funcoes

--- comercio.py
+++ comercio.py
@@ -1,5 +1,19 @@
+from enum import Enum
 
+# Representa as marcas para cada um dos produtos 
+class MarcaChip(Enum):
+    INTEL = 1
+    AMD = 2
 
+class MarcaNotebook(Enum):
+    MACBOOK = 1
+    SAMSUNG = 2
+
+class MarcaInstrument(Enum):
+    GUITAR = 1
+    BASS = 2
+
+# Classe que representa as informações globais de qualquer produto
 class Produto():
     def __init__(self, name, bar_code, price, quantity) -> None:
         self.name = name
@@ -7,45 +21,73 @@ class Produto():
         self.price = price
         self.quantity = quantity
 
-    def show_info(self):
-        ...
+    def show_product(self):
+        print(f"Nome: {self.name}, Código de Barras: {self.bar_code}, Preço: {self.price}, Quantidade: {self.quantity}")
 
-    def decrease_quantity(self, amount):
-        ...
-    
+    def decrease(self, amount):
+        if self.quantity >= amount:
+            self.quantity -= amount
+        else:
+            raise Exception("Quantidade insuficiente em estoque")
+
+# Classes específicas para cada tipo de produto com informações exclusivas para ela
 class Chip(Produto):
-    def __init__(self, name, price, quantity,bar_code) -> None:
-        super().__init__(name, price, quantity,bar_code)
+    def __init__(self, name, bar_code, price, quantity, marca):
+        super().__init__(name, bar_code, price, quantity)
+        self.marca = marca
 
 class Notebook(Produto):
-    def __init__(self, name, price, quantity,bar_code) -> None:
-        super().__init__(name, price, quantity,bar_code)
+    def __init__(self, name, bar_code, price, quantity, marca):
+        super().__init__(name, bar_code, price, quantity)
+        self.marca = marca
 
 class Instrument(Produto):
-    def __init__(self, name, price, quantity,bar_code) -> None:
-        super().__init__(name, price, quantity,bar_code)
+    def __init__(self, name, bar_code, price, quantity, marca):
+        super().__init__(name, bar_code, price, quantity)
+        self.marca = marca
 
+# Classe para gerenciar o inventário
 class Inventario():
-    def __init__(self) -> None:
+    def __init__(self):
         self.products = []
     
-    def add_to_inventory(self,product):
+    # adiciona o produto dentro do inventário.
+    def add_to_inventory(self, product):
         self.products.append(product)
+    
+    # Mostra todos os produtos que pertencem ao inventário
     def check_products_inventory(self):
+        print("Produtos no inventário:")
         for product in self.products:
-            print(product.name)
-    def check_barcode_inventory(self):
+            product.show_product()
+            
+    # Vê se o código de barras dado está associado a algum produto registrado
+    def check_barcode_inventory(self, bar_code):
         for product in self.products:
-            print(product.bar_code)
+            if product.bar_code == bar_code:
+                return product
 
-  
+    def sell_product(self, bar_code, amount):
+        product = self.check_barcode_inventory(bar_code)
+        if product:
+            product.decrease(amount)
+            print(f"Venda de {amount} unidades de {product.name}.")
+        else:
+            raise Exception("Produto não está no inventário")
 
-a = Chip('chip','12','34','anfian#cms')
-b = Instrument('piano','12','3','adf2f#cms')
+# Exemplo:
+a = Chip('Intel Z790', '12345', 12.0, 34, MarcaChip.INTEL)
+b = Notebook('MacBook', '54321', 1200.0, 3, MarcaNotebook.MACBOOK)
+c = Instrument('Guitarra', '67890', 500.0, 1, MarcaInstrument.GUITAR)
+
 inv = Inventario()
 inv.add_to_inventory(a)
 inv.add_to_inventory(b)
-inv.check_products_inventory()
+inv.add_to_inventory(c)
 
-    
-    
+try:
+    inv.sell_product('12345', 12)  
+except Exception as e:
+    print(f"Erro na venda: {e}")
+
+inv.check_products_inventory()
